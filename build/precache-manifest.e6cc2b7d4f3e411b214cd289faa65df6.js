6e7a4b4820d6b82a9f828eaccfd30a0fc12096e3
Evgeniy <kievtechnoshop@gmail.com>
2020-05-13 21:12:15 +0300

    pages

---
+++ build/precache-manifest.e6cc2b7d4f3e411b214cd289faa65df6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "491e2139df5e8ca8d2c1f09b6514501f",
+    "url": "/list_of_items_on_react/index.html"
+  },
+  {
+    "revision": "8c3ef0607bc97e340b93",
+    "url": "/list_of_items_on_react/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "fa00e9d3cd5190e2f979",
+    "url": "/list_of_items_on_react/static/js/2.2805fb4c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/list_of_items_on_react/static/js/2.2805fb4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c3ef0607bc97e340b93",
+    "url": "/list_of_items_on_react/static/js/main.386153ea.chunk.js"
+  },
+  {
+    "revision": "5e06ad5e54271d3b7380",
+    "url": "/list_of_items_on_react/static/js/runtime-main.963d0804.js"
+  }
+]);
